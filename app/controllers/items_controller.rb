8490e2f869b3538f699e4fb5aa1eebd29a5c68a7
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-11-03 14:39:46 -0400

    commit tree 7024310dfee666bfb82d2497ad8b7adb54915563

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(params_items)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def params_items
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(e)
+    render json: { error: e.message }, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find_by!(id: params[:id])
     render json: user, include: :items
+  rescue ActiveRecord::RecordNotFound => e
+    render json: {error: e.message}, status: 404
   end
 
 end
